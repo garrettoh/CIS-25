50b171b27432fe6b2c1caeae2583a734e8967098
garrettoh <garrettraese8@gmail.com>
2025-06-27 11:19:35 -0700

    updated Assignment 7

--- Assignment2/Assignment7/Item.cpp
+++ Assignment2/Assignment7/Item.cpp
@@ -4,11 +4,11 @@ Item::Item(string n, int q) : name(n), quantity(q) {}
 
 Perishable::Perishable(string n, int q) : Item(n, q) {}
 
+NonPerishable::NonPerishable(string n, int q) : Item(n, q) {}
 void Perishable::display() {
     cout << name << " (Perishable), Qty: " << quantity << "\n";
 }
 
-NonPerishable::NonPerishable(string n, int q) : Item(n, q) {}
 
 void NonPerishable::display() {
     cout << name << " (Non-Perishable), Qty: " << quantity << "\n";
